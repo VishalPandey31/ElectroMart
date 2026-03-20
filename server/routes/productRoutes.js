f825939b36e851c961baf98fe404332ee95acdb6
Bubble Wrap <vishalpandey000090@gmail.com>
2026-03-21 01:13:41 +0530

    Fix product routing and restore database data

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,13 +4,22 @@ const {
   getProducts, getProductBySlug, createProduct, updateProduct, deleteProduct,
   getFeaturedProducts, getSearchSuggestions, getRecommendations,
 } = require('../controllers/productController');
+const { getCategories } = require('../controllers/categoryController');
 const { protect, admin, vendor } = require('../middleware/authMiddleware');
 
 router.get('/featured', getFeaturedProducts);
 router.get('/suggestions', getSearchSuggestions);
+router.get('/categories', getCategories);
 router.get('/slug/:slug', getProductBySlug);
-router.route('/').get(getProducts).post(protect, vendor, createProduct);
-router.route('/:id').get(getProductBySlug).put(protect, vendor, updateProduct).delete(protect, admin, deleteProduct);
 router.get('/:id/recommendations', getRecommendations);
+
+// Base products route
+router.get('/', getProducts);
+router.post('/', protect, vendor, createProduct);
+
+// Parameterized product routes
+router.get('/:id', getProductBySlug);
+router.put('/:id', protect, vendor, updateProduct);
+router.delete('/:id', protect, admin, deleteProduct);
 
 module.exports = router;
